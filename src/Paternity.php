f684818ee11a9626c39dd04fc74e06aba9e9a0dc
Jangkar Developer <60224099+jangkar-dev@users.noreply.github.com>
2022-08-16 15:50:15 +0800

    Add files via upload

---
+++ src/Paternity.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jangkardev\Paternity;
+
+class Paternity
+{
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
---
+++ src/PaternityServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Jangkardev\Paternity;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaternityServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
