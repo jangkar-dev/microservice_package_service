72943ca3fe29262ce80f3c2b28f1772249d45916
Jangkar Developer <60224099+jangkar-dev@users.noreply.github.com>
2022-08-20 12:05:10 +0800

    Update Service.php

--- src/Service.php
+++ src/Service.php
@@ -7,4 +7,36 @@ use Illuminate\Support\Facades\Http;
 
 class Service
 {
+    protected $baseUrl;
+    protected $token;
+    public function __construct($type)
+    {
+        $SSL = "https://";
+        $TYPE = strtoupper($type);
+        $API = env($TYPE.'_SERVICE_URL');
+        $TOKEN = env($TYPE.'_SERVICE_TOKEN');
+        if (explode('.', $API)[0] == '127') {
+            $SSL = "http://";
+        }
+        $this->baseUrl = $SSL . $API;
+        $this->token = $TOKEN;
+    }
+    public function getRequest()
+    {
+        return Http::withHeaders([
+            'Authenticated' => json_encode([
+                'user' => config('user'),
+            ]),
+            'Microservice_Token' => $this->token
+        ]);
+    }
+    public function returnRequest($response)
+    {
+        return collect($response->object()->data);
+    }
+    public function get($service, $request = [])
+    {
+        $response = $this->getRequest()->get($this->baseUrl . '/' . $service, $request);
+        return $this->returnRequest($response);
+    }
 }
