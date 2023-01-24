ab458924c1b1f0ae94e88998270145590d67818f
Jangkar Developer <60224099+jangkar-dev@users.noreply.github.com>
2023-01-24 10:47:22 +0800

    Update UserLogTrait.php

--- src/UserLogTrait.php
+++ src/UserLogTrait.php
@@ -2,25 +2,27 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Facades\Session;
+
 trait UserLogTrait
 {
     //query builder
     public function validatedWithUser()
     {
         $input = $this->validated();
-        $input['created_by'] = config('user')->id;
-        $input['updated_by'] = config('user')->id;
+        $input['created_by'] = Session::get('user')->id;
+        $input['updated_by'] = Session::get('user')->id;
         return $input;
     }
     public function validatedWithUserEdit()
     {
         $input = $this->validated();
-        $input['updated_by'] = config('user')->id;
+        $input['updated_by'] = Session::get('user')->id;
         return $input;
     }
     public function validatedWithUserDelete()
     {
-        $input['deleted_by'] = config('user')->id;
+        $input['deleted_by'] = Session::get('user')->id;
         return $input;
     }
 }
